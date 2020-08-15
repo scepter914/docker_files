baa93012bc6b9aa8ea0365f7b1712a2fe7f47d62
scepter914 <st14.828soccer@gmail.com>
2020-08-15 14:53:37 +0900

    update config file

--- config.rb
+++ config.rb
@@ -7,20 +7,7 @@ Precious::App.set(:wiki_options,{
   :allow_uploads => true,
   :mathjax => true,
   :css => true,
-  :js => true,
-  :live_preview => true,
+  :pagination_count => 40,
 })
 
-# table color
-sanitizer = Gollum::Sanitization.new
-sanitizer.attributes[:all].push 'style' # Attributes
-Precious::App.set(:wiki_options, {:sanitization => sanitizer})
-
-# iframe
-sanitizer = Gollum::Sanitization.new
-sanitizer.elements.push 'iframe' # Tag
-sanitizer.attributes['iframe'] = [
-  'width', 'height', 'src', 'frameborder', 'allowfullscreen'
-]  # Attributes
-Precious::App.set(:wiki_options, { :sanitization => sanitizer })
 
